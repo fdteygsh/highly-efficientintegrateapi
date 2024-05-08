6fc61fe3b80c30a0bef7e940688fb368fcd372e7
fdteygsh <gs1kirby623@hotmail.com>
2024-05-08 21:38:08 +0700

    aptos sdk project

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-function findRedundantConnection(edges) {
-  const parent = new Array(edges.length + 1).fill(-1);
-  for (const edge of edges) {
-    const u = find(parent, edge[0]);
-    const v = find(parent, edge[1]);
-    if (u === v) return edge;
-    parent[u] = v;
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) return i;
   }
-  return [];
-}
-function find(parent, i) {
-  if (parent[i] === -1) return i;
-  return find(parent, parent[i]);
+  return -1;
 }
