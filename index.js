99467abdfe65e2d4ec18d297001ecfa3bafd0fc1
fdteygsh <gs1kirby623@hotmail.com>
2024-05-15 05:45:19 +0700

    update aptos

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const deepFlatten = (arr) =>
-  [].concat(...arr.map((v) => (Array.isArray(v) ? deepFlatten(v) : v)));
+function findMaxLength(nums) {
+  const map = new Map();
+  map.set(0, -1);
+  let count = 0;
+  let maxLength = 0;
+  for (let i = 0; i < nums.length; i++) {
+    count += nums[i] === 1 ? 1 : -1;
+    if (map.has(count)) {
+      maxLength = Math.max(maxLength, i - map.get(count));
+    } else {
+      map.set(count, i);
+    }
+  }
+  return maxLength;
+}
